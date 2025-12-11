475381743ad8eea5c4e27794531250c50efd4a09
Clinton Lunn <cpl09c@gmail.com>
2025-12-10 22:40:11 -0700

    refactor: consolidate spatial geometry preparation across sections

--- app/R/reactives.R
+++ app/R/reactives.R
@@ -8,3 +8,19 @@
 build_legend_title <- function(data, label_col = "label") {
   paste0(unique(data[[label_col]])[1], ' (', unique(data$units)[1], ')')
 }
+
+#' Add spatial geometry to data
+#' Joins site attributes and converts to SF object with jitter
+#' @param data Data frame with siteid column
+#' @param site_attr Site attributes data frame with lat/lon
+#' @return SF data frame with geometry
+add_spatial_geometry <- function(data, site_attr) {
+  data %>%
+    left_join(
+      site_attr %>% select(siteid, latitude, longitude, ecoregion, Watershed, `Wetland Type`),
+      by = 'siteid'
+    ) %>%
+    filter(!is.na(longitude) & !is.na(latitude)) %>%
+    st_as_sf(coords = c('longitude', 'latitude'), crs = 4326) %>%
+    st_jitter(amount = 0.001)
+}
